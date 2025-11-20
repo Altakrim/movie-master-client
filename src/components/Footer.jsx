ce7e5a05353e5138d977f1056877f85b46087508
Altakrim <emd34057gmail.com>
2025-11-20 15:50:09 +0600

    Setup responsive navbar and Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { FaFacebook, FaFacebookF, FaInstagramSquare, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className='footer footer-center p-4 bg-base-200 text-base-content mt-10'>
-            <p>&copy; {new Date().getFullYear()} MovieMaster Pro</p>
-        </footer>
+        <footer className="footer p-10 bg-base-200 text-base-content flex justify-between px-20 md:flex-row flex-col items-center">
+           
+  <nav className="" >
+    <span className='footer-title'>Quick Links</span>
+    
+   <div className='flex space-x-4 md:flex-row flex-col space-y-2'>
+     <a className="link link-hover" href='/'>Home</a>
+    <a className="link link-hover" href='/movies'>All Movie</a>
+    <a className="link link-hover" href='/my-collection'>My Collection</a>
+   </div>
+   
+  </nav>
+  <nav>
+           <span className='footer-title mx-3'>Follow Us</span>
+         <div className="flex space-x-4 ">
+            <a href="#"><FaFacebookF className='h-6 w-6'></FaFacebookF></a>
+             <a href="#"><FaTwitter className='h-6 w-6'></FaTwitter></a>
+              <a href="#"><FaInstagramSquare className='h-6 w-6'></FaInstagramSquare></a>
+         </div>
+    
+  </nav>
+   <div className=''>
+                <span className='footer-title'>MovieMaster Pro</span>
+                <p>&copy; {new Date().getFullYear()} MovieMaster Pro</p>
+            </div>
+  
+</footer>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,88 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
     const navLinks = (
         <>
            <li><Link to="/">Home</Link></li>
             <li><Link to="/movies">All Movies</Link></li>
-             <li><Link to="/login">Login</Link></li>
+             <li><Link to="/my-collection">My Collection</Link></li>
+             
         </>
     )
     return (
-        <div className='navbar bg-base-200 px-4'>
-            <div className='navbar-start'>
-                <Link to="/" className='text-xl font-bold'>MovieMaster</Link>
-            </div>
-            <div className='navbar-end'>
-                <ul className='menu menu-horizontal px-1'>
-                  {navLinks}
-                </ul>
-            </div>
-        </div>
+        // <div className='navbar bg-base-200 px-4'>
+        //     <div className='navbar-start'>
+        //         <Link to="/" className='text-xl font-bold sparkle-button'>MovieMaster</Link>
+        //     </div>
+        //     <div className='navbar-center hidden lg:flex'>
+        //         <ul className='menu menu-horizontal px-1'>
+        //           {navLinks}
+        //         </ul>
+        //     </div>
+        //     <div className='navbar-end'>
+        //         {!user ? (
+        //             <>
+        //               <Link to="/login" className='btn btn-sm btn-primary mr-2'>Login </Link>
+        //               <Link to="/register" className='btn btn-sm btn-secondary'>Register</Link>
+        //             </>
+        //         ) : (
+        //             <div className='dropdown dropdown-end'>
+        //                 <label tabIndex={0} className='btn btn-sm btn-primary rounded-btn'>
+        //                     {user.name}
+        //                 </label>
+        //                 <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52'>
+        //                     <li><Link to="/profile">Profile</Link></li>
+        //                     <li><Link to= "/logout">Logout</Link></li>
+        //                 </ul>
+        //             </div>
+        //         )}
+
+        //     </div>
+            
+        // </div>
+ <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {navLinks}
+      </ul>
+    </div>
+    <div className='navbar-start'>
+      <Link to="/" className='text-xl font-bold sparkle-button'>MovieMaster</Link>
+     </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+          {navLinks}
+    </ul>
+  </div>
+  <div className="navbar-end">
+      {!user ? (
+                    <>
+                      <Link to="/login" className='btn btn-sm btn-primary mr-2'>Login </Link>
+                      <Link to="/register" className='btn btn-sm btn-secondary'>Register</Link>
+                    </>
+                ) : (
+                    <div className='dropdown dropdown-end'>
+                        <label tabIndex={0} className='btn btn-sm btn-primary rounded-btn'>
+                            {user.name}
+                        </label>
+                        <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52'>
+                            <li><Link to="/profile">Profile</Link></li>
+                            <li><Link to= "/logout">Logout</Link></li>
+                        </ul>
+                    </div>
+                )}
+  </div>
+</div>
+
+       
     );
 };
 
